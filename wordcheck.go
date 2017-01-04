7212235df207e2a283e469c8baebb8769dcf829f
edwin <edwin.lzh@gmail.com>
2017-01-04 15:19:06 +0800

    fix new wc

--- wordcheck.go
+++ wordcheck.go
@@ -22,6 +22,17 @@ type WcStatus struct {
 	Size int
 }
 
+type WcResult struct {
+	Word  string       `json:"word"`
+	Info  WcResultInfo `json:"info"`
+	Start int64        `json:"start"`
+	Len   int64        `json:"len"`
+}
+
+type WcResultInfo struct {
+	Weight int64 `json:"weight"`
+}
+
 func Create(filename string, flag string, memo string, size int) (*C.wcMM, error) {
 	var table *C.wcTable
 	var err error
@@ -52,10 +63,10 @@ func Status(MM *C.wcMM) *WcStatus {
 	return ret
 }
 
-func Check(MM *C.wcMM, data string) (map[int]string, error) {
+func Check(MM *C.wcMM, data string) ([]WcResult, error) {
 	var mmtable *C.wcmmTable
 	var err error
-	ret := make(map[int]string)
+	ret := make([]WcResult, 0)
 	if C.wordcheck_mmtable_fetch(MM, &mmtable) == C.WORDCHECK_SUCCESS {
 		var list *C.wcList
 		var out *C.char
@@ -64,9 +75,14 @@ func Check(MM *C.wcMM, data string) (map[int]string, error) {
 		if num := C.wordcheck_mm_check(MM, mmtable, Cdata, (C.int)(C.strlen(Cdata)), &out, &out_len, &list); num > 0 {
 			var lt *C.wcList
 			var res *C.wcResult
+			var wcResult WcResult
 			for i := 0; C.wordcheck_list_get_current(list, &lt) == C.WORDCHECK_SUCCESS; i++ {
 				res = (*C.wcResult)(lt.val)
-				ret[i] = C.GoString(res.string)
+				wcResult.Word = C.GoString(res.string)
+				wcResult.Start = int64(res.start)
+				wcResult.Len = int64(res.len)
+				wcResult.Info.Weight = int64(res.info.weight)
+				ret = append(ret, wcResult)
 				//fmt.Printf("-->禁词: %s\n", C.GoString(res.string))
 				//fmt.Printf("-->起始: %d\n", res.start)
 				//fmt.Printf("-->长度: %d\n\n", res.len)
